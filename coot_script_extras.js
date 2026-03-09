a2970a35accb4680cf25d5c96b14ad381e0ed234
bernhardcl <b.lohkamp@gmail.com>
2026-03-09 16:25:06 +0100

    Update download and release page
    Download page shows button for latest 0.9 now too.
    Release page separates 1. and 0.9 and previous builds.
    Access to download stats by param ?dev=true

--- coot_script_extras.js
+++ coot_script_extras.js
@@ -35,93 +35,117 @@ function GetLatestReleaseInfo() {
       });
 }
 
-function GetReleases(repo, pre_release) {
+
+function GetReleases(repo, preReleaseFilter, versionFilter, showDownloadCounts) {
+   // preReleaseFilter: "releases" (only stable), "prereleases" (only pre-releases), "all" (both)
+   // versionFilter: "all", "0.9", "1.x" (or "latest" for non-0.9)
+   // showDownloadCounts: true/false (default true)
+
+   if (typeof showDownloadCounts === 'undefined') {
+      showDownloadCounts = true;
+   }
+
    $.getJSON("https://api.github.com/repos/" + repo + "/releases").done(function(json) {
-         var totalDownloadCount = 0;
-         for (var i = 0; i < json.length; i++) {
-            var release = json[i];
-            if (release.assets.length === 0) {
-               continue;
-            }
-            var asset = release.assets[0];
-            var fileSize = Math.round(asset.size / (1024*1024));
-            var downloadCount = 0;
-            for (var i2 = 0; i2 < release.assets.length; i2++) {
-               downloadCount += release.assets[i2].download_count;
-            }
-            var md5sum_url = "dummy";
-            for (var j=0; j <release.assets.length; j++) {
-               var asset2 = release.assets[j];
-               var md5sum = asset.name + ".md5sum";
-               if (asset2.name == md5sum) {
-                  md5sum_url = asset2.browser_download_url;
-               }
-            }
+      var totalDownloadCount = 0;
 
-            totalDownloadCount += downloadCount;
-            // to avoid an extra library (moment.js)
-            var d = new Date(asset.updated_at);
+      for (var i = 0; i < json.length; i++) {
+         var release = json[i];
 
-            var isPre_release = (pre_release == 'true');
+         if (release.assets.length === 0) {
+            continue;
+         }
 
-            if ((pre_release == "all") ||
-                (isPre_release == release.prerelease)) {
-               $(".table-downloads tbody")
-                  .append($("<tr>")
-                          .append($("<td>")
-                                  .append($("<a>")
-                                          .attr("href", release.html_url)
-                                          .text(release.name)
-                                          )
-                                  )
-                          .append($("<td>")
-                                  .append($("<a>")
-                                          .attr("href", asset.browser_download_url)
-                                          .text("download installer")
-                                          )
-                                  )
-                          .append($("<td>")
-                                  .append($("<a>")
-                                          .attr("href", md5sum_url)
-                                          .text("get md5sum")
-                                          )
-                                  )
-                          .append($("<td>")
-                                  .append($("<a>")
-                                          //                                .attr("href", asset.browser_download_url)
-                                          .text(fileSize.toLocaleString() + " MB")
-                                          )
-                                  )
-                          //                        .append($("<td>")
-                          //                            .text(downloadCount.toLocaleString())
-                          //                            )
-                          .append($("<td>")
-                                  .text(d.toLocaleString())
-                                  //                            .text(moment(asset.updated_at).format("YYYY-MM-DD HH:mm"))
-                                  )
-                          .append($("<td>")
-                                  .text(downloadCount.toLocaleString())
-                                  )
-                          );
+         // Pre-release filtering
+         var includeByPreRelease = false;
+         if (preReleaseFilter === "all") {
+            includeByPreRelease = true;
+         } else if (preReleaseFilter === "releases") {
+            includeByPreRelease = !release.prerelease;
+         } else if (preReleaseFilter === "prereleases") {
+            includeByPreRelease = release.prerelease;
+         }
+
+         if (!includeByPreRelease) {
+            continue;
+         }
+
+         // Version filtering
+         var includeByVersion = false;
+         // Extract major version (first number after common prefixes)
+         var versionMatch = release.tag_name.match(/(\d+)\.\d+/) ||
+                            release.name.match(/(\d+)\.\d+/);
+         var majorVersion = versionMatch ? versionMatch[1] : null;
+         if (versionFilter === "all") {
+            includeByVersion = true;
+         } else if (versionFilter === "0.9") {
+            includeByVersion = (majorVersion === "0");
+         } else if (versionFilter === "1.x" || versionFilter === "latest") {
+            includeByVersion = (majorVersion === "1");
+         }
+
+         if (!includeByVersion) {
+            continue;
+         }
+
+         var asset = release.assets[0];
+         var fileSize = Math.round(asset.size / (1024*1024));
+         var downloadCount = 0;
+
+         for (var i2 = 0; i2 < release.assets.length; i2++) {
+            downloadCount += release.assets[i2].download_count;
+         }
+
+         var md5sum_url = "dummy";
+         for (var j = 0; j < release.assets.length; j++) {
+            var asset2 = release.assets[j];
+            var md5sum = asset.name + ".md5sum";
+            if (asset2.name == md5sum) {
+               md5sum_url = asset2.browser_download_url;
             }
          }
 
-         //         if (totalDownloadCount > 0)
-         //  {
-         //      $(".total-downloads").text(" (" + totalDownloadCount.toLocaleString() + " downloads)");
-         //   }
+         totalDownloadCount += downloadCount;
 
-         $(".table-downloads").show();
-         //         console.log($(".table-downloads").prop('outerHTML'));
-      });
-}
+         var d = new Date(asset.updated_at);
 
-function change_myselect(sel) {
-   // clean the table
-   $(".table-downloads tbody").empty();
+         var row = $("<tr>")
+            .append($("<td>")
+               .append($("<a>")
+                  .attr("href", release.html_url)
+                  .text(release.name)
+               )
+            )
+            .append($("<td>")
+               .append($("<a>")
+                  .attr("href", asset.browser_download_url)
+                  .text("download installer")
+               )
+            )
+            .append($("<td>")
+               .append($("<a>")
+                  .attr("href", md5sum_url)
+                  .text("get md5sum")
+               )
+            )
+            .append($("<td>")
+               .append($("<a>")
+                  .text(fileSize.toLocaleString() + " MB")
+               )
+            )
+            .append($("<td>")
+               .text(d.toLocaleString())
+            );
 
-   $(document).ready(function() {
-         repo = "bernhardcl/coot";
-         GetReleases(repo, sel);
-      });
+         // Conditionally add download count column
+         if (showDownloadCounts) {
+            row.append($("<td>")
+               .text(downloadCount.toLocaleString())
+            );
+         }
+
+         $(".table-downloads tbody").append(row);
+      }
+
+      $(".table-downloads").show();
+   });
 }
